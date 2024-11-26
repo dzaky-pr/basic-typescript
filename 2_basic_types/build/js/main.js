87c3bd5b3bb74998c58adfb1352a800721462f1f
dzaky-pr <dzaky.pr.mulyono97@gmail.com>
2024-11-26 23:32:29 +0700

    learn basic types

---
+++ 2_basic_types/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = "Dzaky";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Dzaky Muhammad";
+meaningOfLife = 42;
+isLoading = true;
+album = 1998;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ 2_basic_types/src/main.ts
@@ -0,0 +1,18 @@
+let myName: string = "Dzaky";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "Dzaky Muhammad";
+meaningOfLife = 42;
+isLoading = true;
+album = 1998;
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
